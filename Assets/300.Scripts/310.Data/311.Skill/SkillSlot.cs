03c2df43ab22fa2217b1b0cbe94fc1979b24e4ca
MoonHoYoung <march23mm19@gmail.com>
2025-03-18 13:15:53 +0900

    스킬 슬롯 키 설정 값 적용

--- Assets/300.Scripts/310.Data/311.Skill/SkillSlot.cs
+++ Assets/300.Scripts/310.Data/311.Skill/SkillSlot.cs
@@ -2,16 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
-public class SkillSlot : MonoBehaviour
+public class SkillSlot : MonoBehaviour, IPointerClickHandler
 {
-    public string skillName;
+    public string skillName = string.Empty;
     int slotIndex = 0;
 
     Image img;
     private void Awake()
     {
-        skillName = string.Empty;
         img = transform.GetChild(0).GetComponent<Image>();
         slotIndex = transform.GetSiblingIndex();
         SkillSlotNum(slotIndex);
@@ -20,7 +20,64 @@ public class SkillSlot : MonoBehaviour
 
     void SkillSlotNum(int index)
     {
+        switch(index)
+        {
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            case 6:
+                break;
+        }
+    }
 
+    void Update()
+    {
+        SkillSlotInput(slotIndex);
+    }
+
+    void SkillSlotInput(int index)
+    {
+        if (skillName != string.Empty)
+        {
+            switch (index)
+            {
+                case 2:
+                    if (Input.GetKeyDown(GameManager.data.keyMappings[CustomKeyCode.ShortcutKey3]))
+                    {
+                        Debug.Log(2);
+                    }
+                    break;
+                case 3:
+                    if (Input.GetKeyDown(GameManager.data.keyMappings[CustomKeyCode.ShortcutKey4]))
+                    {
+                        Debug.Log(3);
+                    }
+                    break;
+                case 4:
+                    if (Input.GetKeyDown(GameManager.data.keyMappings[CustomKeyCode.ShortcutKey5]))
+                    {
+
+                    }
+                    break;
+                case 5:
+                    if (Input.GetKeyDown(GameManager.data.keyMappings[CustomKeyCode.ShortcutKey6]))
+                    {
+
+                    }
+                    break;
+                case 6:
+                    if (Input.GetKeyDown(GameManager.data.keyMappings[CustomKeyCode.ShortcutKey7]))
+                    {
+
+                    }
+                    break;
+            }
+        }
     }
 
     public void SkillUIZero()
@@ -30,8 +87,19 @@ public class SkillSlot : MonoBehaviour
         skillName = string.Empty;
     }
 
-    private void Update()
+    public void OnPointerClick(PointerEventData eventData)
     {
-        
+        if (eventData.button == PointerEventData.InputButton.Right) // 마우스 오른쪽 클릭
+        {
+            if(skillName != string.Empty)
+            {
+                SkillData skillToEquip = PlayerManager.instance.player.skill.accquisitionSkillDataList.Find(skill => skill.nameKor == skillName);
+                if(skillToEquip != null)
+                {
+                    skillToEquip.equipPostion = SkillEquipPosition.None;
+                    SkillUIZero();
+                }
+            }
+        }
     }
 }
